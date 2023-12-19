de12661284e231509611f84a4fae7912a9d8a03b
SilviMarku <marku.silvia777@gmail.com>
2023-12-19 13:05:42 +0100

    A

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -12,12 +12,12 @@ from sys import argv
 
 if __name__ == "__main__":
     URL = "https://jsonplaceholder.typicode.com/"
-    user_id = argv[1]
+    user_id = int(argv[1])
     res = requests.get(f"{URL}users/{argv[1]}")
     res = res.json()
-    user_name = res['name']
+    user_name = data.get("name")
 
-    res = requests.get(f"{URL}todos")
+    res = requests.get(f"{URL}/todos")
     all_todos = res.json()
     user_todos = [todo for todo in all_todos if todo['userId'] == int(argv[1])]
     nr_tasks = len(user_todos)
